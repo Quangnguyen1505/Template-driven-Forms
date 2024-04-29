7b53817f70b91de41aa693af433a178a19f85bd0
ThanhQuang-1405 <quang0706r@gmail.com>
2024-04-29 15:33:02 +0700

    first commit

---
+++ src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
+})
+export class SignInComponent {
+  usernamePattern = /^[a-z]{6,32}$/i;
+  passwordPattern = /^(?=.*[!@#$%^&*]+)[a-z0-9!@#$%^&*]{6,32}$/;
+  userInfo = {
+    userName: '',
+    password: '',
+    rememberMe: true,
+  };
+  onSubmit(form: NgForm){
+    console.log(form);
+  }
+}
